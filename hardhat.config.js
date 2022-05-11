5259e9f445a00415fe103c23600f660454609915
hasan <ha5anmukhli50@gmail.com>
2022-05-11 11:33:08 +0800

    WIP

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,14 +10,14 @@ module.exports = {
   paths: {
     sources: "./src/flat"
   },
-  networks: {
-    arbitrumTestnet: {
-      url: "https://rinkeby.arbitrum.io/rpc",
-      accounts: [process.env.PRIVATE_KEY]
-    },
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/0e7fcc143f894d179aa51dbdc44d8ac5",
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+  // networks: {
+  //   arbitrumTestnet: {
+  //     url: "https://rinkeby.arbitrum.io/rpc",
+  //     accounts: [process.env.PRIVATE_KEY ?? ]
+  //   },
+  //   rinkeby: {
+  //     url: "https://rinkeby.infura.io/v3/0e7fcc143f894d179aa51dbdc44d8ac5",
+  //     accounts: [process.env.PRIVATE_KEY]
+  //   }
+  // }
 };
